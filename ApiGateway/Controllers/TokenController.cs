a849b7506d3d3235f0df9be99614b0219cfc8fc2
ben <moon791017@gmail.com>
2021-03-26 11:08:41 +0800

    add token controller

---
+++ ApiGateway/Controllers/TokenController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ApiGateway.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TokenController : ControllerBase
+    {
+    
+
+        private readonly ILogger<TokenController> _logger;
+        private IConfiguration _configuration;
+        public TokenController(ILogger<TokenController> logger,IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration=configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            #region 加入jwt claims 角色授權
+
+
+            #endregion 加入jwt claims 角色授權
+
+            var token = new JwtSecurityToken(
+                // issuer: _configuration.GetValue<string>("Tokens:ValidIssuer"),
+                // audience: _configuration.GetValue<string>("Tokens:ValidAudience"),
+                expires: DateTime.Now.AddDays(365),
+                // claims: claims,
+                signingCredentials: new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetValue<string>("JWTSecretKey"))),
+                    SecurityAlgorithms.HmacSha256)
+            );
+            // token.Payload.Add("UserInfo", model);
+            var SecurityToken = (new JwtSecurityTokenHandler().WriteToken(token));
+            return Ok(SecurityToken);
+        }
+    }
+}
