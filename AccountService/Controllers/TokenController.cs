7031c1b9d76a190babb0dacadaf354d0650865fa
ben <moon791017@gmail.com>
2021-04-22 11:29:01 +0800

    OK

---
+++ AccountService/Controllers/TokenController.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using AccountService.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AccountService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TokenController : ControllerBase
+    {
+
+
+        private readonly ILogger<TokenController> _logger;
+        private IdentityService _identityService;
+        public TokenController(ILogger<TokenController> logger,IdentityService identityService)
+        {
+            _logger = logger;
+            _identityService=identityService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var Token=await _identityService.GetToken();
+            return Ok(Token);
+        }
+    }
+}
--- AccountService/Services/IdentityService.cs
+++ AccountService/Services/IdentityService.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace AccountService.Services
@@ -6,13 +10,34 @@ namespace AccountService.Services
     public class IdentityService
     {
         private ILogger<IdentityService> _logger;
-        public HttpClient _client { get; }
+        private HttpClient _client { get; }
+        private IConfiguration _config;
         public IdentityService(
            ILogger<IdentityService> logger,
-           HttpClient client)
+           HttpClient client,IConfiguration config)
         {
-            _client = client; ;
             _logger = logger;
+            _client = client;
+            _config=config;
+        }
+        public async Task<string> GetToken()
+        {
+            var nvc = new List<KeyValuePair<string, string>>();
+            nvc.Add(new KeyValuePair<string, string>("grant_type", "client_credentials"));
+            nvc.Add(new KeyValuePair<string, string>("client_id", _config.GetValue<string>("CLIENT")));
+            nvc.Add(new KeyValuePair<string, string>("client_secret", _config.GetValue<string>("SECRET")));
+            string url=_config.GetValue<string>("IDENTITY_SERVER");
+            var req = await _client.PostAsync($"{url}/connect/token", new FormUrlEncodedContent(nvc));
+            try
+            {
+                var data = await req.Content.ReadAsStringAsync();
+                return data;
+            }
+            catch (System.Exception e)
+            {
+                _logger.LogError(e.Message);
+                return string.Empty;
+            }
         }
     }
 }
